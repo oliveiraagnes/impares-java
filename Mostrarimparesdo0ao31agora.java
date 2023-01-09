65d5b24311d3d45c8f82f377217be5a08765b8d5
Agnes Oliveira <110440570+oliveiraagnes@users.noreply.github.com>
2023-01-09 18:48:21 -0300

    Arquivo Principal

---
+++ Mostrarimparesdo0ao31agora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg31_impares;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int num = 0;
+        while(num<=31) {
+             if(num%2 != 0){
+            System.out.println("Números Ímpares: "+num);;
+                         }
+            num++;
+           
+                    
+		
+		
+	      
+      }
+ }
+}
+
